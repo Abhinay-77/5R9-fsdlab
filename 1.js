d428816aa4ee4c6906202aca020d4af202258f3c
Abhinay-77 <allamabhinay03@gmail.com>
2025-01-18 14:42:45 +0530

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,10 +1,33 @@
-function mergeArrays(...args){
-    let arr=new Array();
+let x=20;
+console.log(x);
 
-    let l=args.length;
-    for(let i=0;i<l;i++){
-        arr=[...arr,...args[i]];
-    }
-    console.log(arr);
-}
-mergeArrays([1,2],[3,4],[5,6]);
+x=10;
+console.log(x + " "+typeof x);
+
+let name="abhi";
+console.log(name +" "+typeof name);
+
+let crct=true;
+console.log(crct+" "+typeof crct);
+
+//var
+
+console.log(y);
+var y=20;
+console.log(y);
+y=10;
+console.log(y + " "+typeof y);
+var name1 ="jai";
+console.log(name1 +" "+typeof name1);
+var crct1=false;
+console.log(crct1+" "+typeof crct1);
+
+//const
+const z=30;
+console.log(z);
+console.log(typeof z);
+
+const name2="raghu";
+console.log(name2+" "+typeof name2);
+const crct2=true;
+console.log(crct2+" "+typeof crct2);
--- t31.js
+++ t31.js
@@ -1,10 +1,20 @@
-function mergeArrays(...args){
-    let arr=new Array();
+function merge(...args){
+
+    let arr = new Array();
 
     let l=args.length;
-    for(let i=0;i<l;i++){
-        arr=[...arr,...args[i]];
-    }
-    console.log(arr);
+
+     for(let i=0;i<l;i++){
+     
+
+    
+    arr=[...arr,...args[i]]
+    
+      }
+      console.log(arr);
+
+
 }
-mergeArrays([1,2],[3,4],[5,6]);
+
+merge([1,2],[3,4]);
+merge([1,2],[3,4],[5,6]);
